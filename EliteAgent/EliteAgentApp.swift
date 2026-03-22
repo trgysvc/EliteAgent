b465f020b38758a6d866268e15b8f723b3ebc2c9
Turgay Savacı <turgysavaci@gmail.com>
2026-03-22 20:36:44 +0300

    Initial Commit

---
+++ EliteAgent/EliteAgentApp.swift
@@ -0,0 +1,17 @@
+//
+//  EliteAgentApp.swift
+//  EliteAgent
+//
+//  Created by Turgay Savacı on 3/22/26.
+//
+
+import SwiftUI
+
+@main
+struct EliteAgentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ EliteAgentTests/EliteAgentTests.swift
@@ -0,0 +1,17 @@
+//
+//  EliteAgentTests.swift
+//  EliteAgentTests
+//
+//  Created by Turgay Savacı on 3/22/26.
+//
+
+import Testing
+@testable import EliteAgent
+
+struct EliteAgentTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
